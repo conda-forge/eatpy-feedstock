5b0e620738e58023b5367991a3b045b6e01663d5
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-05-01 15:17:21 +0100

    adopt conda-forge knowledge base recommendations for cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,5 +7,9 @@ else
   CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
-export OPAL_PREFIX=${PREFIX}
+# As per https://conda-forge.org/docs/maintainer/knowledge_base/#cross-compilation-examples
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && "${mpi}" == "openmpi" ]]; then
+  export OPAL_PREFIX="$PREFIX"
+fi
+
 source "$SRC_DIR/install" -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" -DFABM_EXTRA_INSTITUTES=ogs -DFABM_OGS_BASE="$SRC_DIR/extern/ogs" ${CMAKE_PLATFORM_FLAGS[@]}
