06d0c282932e204d16922bcc2ca958253d2d07c9
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-05-01 15:33:11 +0100

    force use of openmpi compiler wrappers when cross compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,6 +11,9 @@ fi
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && "${mpi}" == "openmpi" ]]; then
   export OPAL_PREFIX="$PREFIX"
   export OMPI_FCFLAGS=${FFLAGS}
+  export CC=mpicc
+  export CXX=mpic++
+  export FC=mpifort
 fi
 
 source "$SRC_DIR/install" -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" -DFABM_EXTRA_INSTITUTES=ogs -DFABM_OGS_BASE="$SRC_DIR/extern/ogs" ${CMAKE_PLATFORM_FLAGS[@]}
