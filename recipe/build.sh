6df8b22c236daaf3fa051003fef3c75ce02742d1
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-05-01 16:05:54 +0100

    always export compiler wrappers

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,9 +11,10 @@ fi
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && "${mpi}" == "openmpi" ]]; then
   export OPAL_PREFIX="$PREFIX"
   #export OMPI_FCFLAGS=${FFLAGS}
-  export CC=mpicc
-  export CXX=mpic++
-  export FC=mpifort
 fi
+
+export CC=mpicc
+export CXX=mpic++
+export FC=mpifort
 
 source "$SRC_DIR/install" -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" -DFABM_EXTRA_INSTITUTES=ogs -DFABM_OGS_BASE="$SRC_DIR/extern/ogs" ${CMAKE_PLATFORM_FLAGS[@]}
